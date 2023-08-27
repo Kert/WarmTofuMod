3a90330ed919bfad3556d5f4fd3ce7a60e0f11a5
Kert <kertwaii@gmail.com>
2023-08-27 19:15:24 +0600

    Replaced Lobby button with Teleport button that can take you Downhill or Uphill

--- WarmTofuMod.cs
+++ WarmTofuMod.cs
@@ -6,12 +6,14 @@ using CodeStage.AntiCheat.Storage;
 using CodeStage.AntiCheat.ObscuredTypes;
 using System.Collections.Generic;
 using MonoMod.Cil;
-using BepInEx.Logging;
 using Photon.Pun;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 namespace WarmTofuMod
 {
-    [BepInPlugin("com.kert.warmtofumod", "WarmTofuMod", "1.3.2")]
+    [BepInPlugin("com.kert.warmtofumod", "WarmTofuMod", "1.4.0")]
     public class WarmTofuMod : BaseUnityPlugin
     {
         public enum Menus
@@ -73,6 +75,13 @@ namespace WarmTofuMod
         };
 
         const BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+        GameObject teleportMenu = new GameObject();
+        enum TeleportPoints
+        {
+            TP_Downhill,
+            TP_Uphill,
+            TP_Downhill2
+        }
 
         private void Awake()
         {
@@ -124,6 +133,9 @@ namespace WarmTofuMod
                 On.EnterAreaGarage.Update += EnterAreaGarage_Update;
                 On.SRTransitionMap.Update += SRTransitionMap_Update;
                 IL.SRUIManager.Update += SRUIManager_Update;
+                On.SRUIManager.Start += SRUIManager_Start;
+                On.SRUIManager.OpenMenu += SRUIManager_OpenMenu;
+                On.SRCusorManager.Update += SRCusorManager_Update;
 
                 // missing lights fix
                 On.RCC_LightEmission.Update += RCC_LightEmission_Update;
@@ -497,6 +509,157 @@ namespace WarmTofuMod
                     self.TopCamera.SetActive(true);
                     typeof(SRPlayerFonction).GetField("OK", bindingFlags).SetValue(self, false);
                 }
+            }
+
+
+            void SRUIManager_Start(On.SRUIManager.orig_Start orig, SRUIManager self)
+            {
+                orig(self);
+
+                if (SceneManager.GetActiveScene().name == "DriftSekai")
+                    return;
+
+                // Change lobby button behaviour
+                Button lobby = self.BtnLobby.gameObject.GetComponent<Button>();
+                lobby.onClick = new Button.ButtonClickedEvent();
+
+                teleportMenu = Instantiate(self.MenuExit);
+                Transform transform = teleportMenu.transform;
+                transform.SetParent(self.MenuGeneral.transform.GetParent());
+                transform.position = self.BtnLobby.transform.position;
+                transform.localScale = new Vector3(1f, 1f, 1f);
+
+                teleportMenu.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0, 0, 0);
+                teleportMenu.GetComponentInChildren<Text>().text = "Teleport";
+                Button[] tpButtons = teleportMenu.GetComponentsInChildren<Button>();
+                tpButtons[0].GetComponentInChildren<Text>().text = "Downhill";
+                tpButtons[1].GetComponentInChildren<Text>().text = "Uphill";
+                tpButtons[0].onClick = new Button.ButtonClickedEvent();
+                tpButtons[1].onClick = new Button.ButtonClickedEvent();
+                tpButtons[0].onClick.AddListener(() => TeleportPlayer(TeleportPoints.TP_Downhill));
+                tpButtons[1].onClick.AddListener(() => TeleportPlayer(TeleportPoints.TP_Uphill));
+
+                lobby.onClick.AddListener(TeleportMenu);
+            }
+
+            void SRUIManager_OpenMenu(On.SRUIManager.orig_OpenMenu orig, SRUIManager self)
+            {
+                orig(self);
+                self.BtnLobby.GetComponentInChildren<Text>().text = "Teleport";
+                teleportMenu.SetActive(false);
+            }
+
+            void SRCusorManager_Update(On.SRCusorManager.orig_Update orig, SRCusorManager self)
+            {
+                orig(self);
+                if (teleportMenu && teleportMenu.activeSelf)
+                {
+                    Cursor.visible = true;
+                    prefsInt["MenuOpen"] = 1;
+                }
+            }
+
+            IEnumerator TeleportPlayerVehicle(Vector3 pos, Quaternion rot)
+            {
+                RCC_CarControllerV3 vehicle = RCC_SceneManager.Instance.activePlayerVehicle;
+                GameObject gameObject = vehicle.gameObject;
+                Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+                Transform t = gameObject.transform;
+                rb.isKinematic = true;
+                rb.velocity = rb.angularVelocity = new Vector3(0f, 0f, 0f);
+                t.position = pos;
+                t.rotation = rot;
+                vehicle.currentGear = 0;
+                vehicle.KillEngine();
+                vehicle.StartEngine();
+                GameObject[] cams = GameObject.FindGameObjectsWithTag("cam");
+                foreach (GameObject cam in cams)
+                {
+                    cam.GetComponent<RCC_Camera>().ResetCamera();
+                }
+                yield return new WaitForSeconds(0.1f);
+                rb.isKinematic = false;
+                yield break;
+            }
+
+            void TeleportPlayer(TeleportPoints tp)
+            {
+                Vector3 pos = new();
+                Quaternion rot = new();
+                string map = SceneManager.GetActiveScene().name;
+                switch (map)
+                {
+                    case "Irohazaka":
+                        switch (tp)
+                        {
+                            case TeleportPoints.TP_Downhill:
+                                pos = new Vector3(1309.3f, 258.8f, 167.9f);
+                                rot = new Quaternion(0.0f, -1.0f, 0.0f, -0.1f);
+                                break;
+                            case TeleportPoints.TP_Uphill:
+                                pos = new Vector3(-1327.4f, -290.8f, 238.6f);
+                                rot = new Quaternion(0.0f, 0.9f, 0.0f, 0.4f);
+                                break;
+                            case TeleportPoints.TP_Downhill2:
+                                pos = new Vector3(-285.0f, 204.4f, 505.8f);
+                                rot = new Quaternion(0.0f, 0.0f, 0.0f, -1.0f);
+                                break;
+                        }
+                        break;
+                    case "Akagi":
+                        switch (tp)
+                        {
+                            case TeleportPoints.TP_Downhill:
+                                pos = new Vector3(-250.4f, 133.4f, -777.3f);
+                                rot = new Quaternion(0.0f, -0.8f, 0.0f, -0.6f);
+                                break;
+                            case TeleportPoints.TP_Uphill:
+                                pos = new Vector3(716.5f, -132.5f, 359.6f);
+                                rot = new Quaternion(0.0f, -0.4f, 0.0f, 0.9f);
+                                break;
+                        }
+                        break;
+                    case "USUI":
+                        switch (tp)
+                        {
+                            case TeleportPoints.TP_Downhill:
+                                pos = new Vector3(1316.5f, 66.2f, 777.4f);
+                                rot = new Quaternion(0.0f, -0.7f, 0.0f, 0.7f);
+                                break;
+                            case TeleportPoints.TP_Uphill:
+                                pos = new Vector3(-1593.9f, -212.8f, -532.2f);
+                                rot = new Quaternion(0.0f, -0.3f, 0.0f, 0.9f);
+                                break;
+                        }
+                        break;
+                    case "Akina":
+                        switch (tp)
+                        {
+                            case TeleportPoints.TP_Downhill:
+                                pos = new Vector3(917.0f, 136.7f, 1359.4f);
+                                rot = new Quaternion(0.0f, 1.0f, 0.0f, -0.1f);
+                                break;
+                            case TeleportPoints.TP_Uphill:
+                                pos = new Vector3(-1218.8f, -135.7f, -1187.5f);
+                                rot = new Quaternion(0.0f, 1.0f, 0.0f, 0.0f);
+                                break;
+                        }
+                        break;
+                }
+
+                RaceManager rm = GameObject.FindWithTag("RaceManager").GetComponent<RaceManager>();
+                rm.StopRun();
+                rm.ToffuManager.GetComponent<SRToffuManager>().StopDelivery();
+                this.StartCoroutine(TeleportPlayerVehicle(pos, rot));
+                teleportMenu.SetActive(false);
+                prefsInt["MenuOpen"] = 0;
+            }
+
+            void TeleportMenu()
+            {
+                teleportMenu.SetActive(true);
+                teleportMenu.GetComponentInChildren<Button>().Select();
+                GameObject.FindObjectOfType<SRUIManager>().MenuGeneral.SetActive(false);
             }
 
             void GarageManager_Start(On.GarageManager.orig_Start orig, GarageManager self)
